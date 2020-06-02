ee15d21cc9eff044c98e34995d120dcfc039544e
Galder Zamarreño <galder@zamarreno.com>
2020-06-02 18:53:13 +0200

    Update default JDK source tree when doing graal build

--- src/qollider.java
+++ src/qollider.java
@@ -350,7 +350,7 @@ class GraalBuild implements Runnable
     private final Consumer<Options> runner;
 
     @Option(
-        defaultValue = "https://github.com/graalvm/labs-openjdk-11/tree/jvmci-20.0-b02"
+        defaultValue = "https://github.com/graalvm/labs-openjdk-11/tree/jvmci-20.2-b01"
         , description = "JDK source tree URL"
         , names =
         {
