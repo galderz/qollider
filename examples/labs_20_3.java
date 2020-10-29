b64fa3dce7c5dcb838aed67e45b75634f7f8ee08
Galder Zamarreño <galder@redhat.com>
2020-10-29 11:04:10 +0100

    Add qollider example to build Labs JDK 20.3

---
+++ examples/labs_20_3.java
@@ -0,0 +1,29 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 14
+//JAVAC_OPTIONS --enable-preview -source 14
+//JAVA_OPTIONS --enable-preview
+//DEPS org.mendrugo.qollider:qollider:${qollider.version:0.1.0}
+
+import org.mendrugo.qollider.Jdk;
+import org.mendrugo.qollider.Qollider;
+import org.mendrugo.qollider.Repository;
+
+/**
+ * Script to build labs JDK 20.3.
+ */
+public class labs_20_3
+{
+    public static void main(String... args)
+    {
+        final var qollider = Qollider.of();
+        qollider
+            .plan(
+                qollider.jdk().build(
+                    new Jdk.Build(
+                        Repository.of("https://github.com/graalvm/labs-openjdk-11/tree/jvmci-20.3-b03")
+                    )
+                )
+            )
+            .run();
+    }
+}
