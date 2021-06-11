166bad8f9402eef71538aefc6ad8aa3893a6ed38
Galder Zamarreño <galder@zamarreno.com>
2021-06-11 15:11:49 +0200

    Update examples to 0.0.10

--- examples/graal.java
+++ examples/graal.java
@@ -3,7 +3,7 @@
 //JAVAC_OPTIONS --enable-preview -source 16
 //JAVA_OPTIONS --enable-preview
 //DEPS info.picocli:picocli:4.5.2
-//DEPS org.mendrugo.qollider:qollider:${qollider.version:0.0.9}
+//DEPS org.mendrugo.qollider:qollider:${qollider.version:0.0.10}
 
 import org.mendrugo.qollider.Graal;
 import org.mendrugo.qollider.Jdk;
